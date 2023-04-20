481d8b5a5ebecb32d80eb387288da90944f0dbfc
sigetg <sigetyg@bc.edu>
2023-04-19 21:07:02 -0400

    Initial Commit

---
+++ TapPlease!/TapPlease_App.swift
@@ -0,0 +1,17 @@
+//
+//  TapPlease_App.swift
+//  TapPlease!
+//
+//  Created by George Sigety on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TapPlease_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
